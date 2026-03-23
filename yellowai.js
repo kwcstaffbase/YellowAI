73b61c661d8d20d3f4882590708f17e9309e2b17
kwcstaffbase <kelvin.cheng@staffbase.com>
2026-03-23 16:15:04 -0600

    Refactor YellowMessenger initialization logic

--- yellowai.js
+++ yellowai.js
@@ -2,20 +2,34 @@ window.ymConfig = {"bot":"x1765817669787","host":"https://r4.cloud.yellow.ai"};
 (function() {
     var w = window,
         ic = w.YellowMessenger;
-    if ("function" === typeof ic) ic("reattach_activator"), ic("update", ymConfig);
-    else {
+    if ("function" === typeof ic) {
+        ic("reattach_activator");
+        ic("update", ymConfig);
+    } else {
         var d = document,
             i = function() {
-                i.c(arguments)
+                i.c(arguments);
             };
         function l() {
             var e = d.createElement("script");
-            e.type = "text/javascript", e.async = !0, e.src = "https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js";
+            e.type = "text/javascript";
+            e.async = true;
+            e.src = "https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js";
             var t = d.getElementsByTagName("script")[0];
-            t.parentNode.insertBefore(e, t)
+            t.parentNode.insertBefore(e, t);
         }
-        i.q = [], i.c = function(e) {
-            i.q.push(e)
-        }, w.YellowMessenger = i, w.attachEvent ? w.attachEvent("onload", l) : w.addEventListener("load", l, !1)
+        i.q = [];
+        i.c = function(e) {
+            i.q.push(e);
+        };
+        w.YellowMessenger = i;
+
+        // If the page already loaded, call l() immediately
+        // Otherwise, wait for the load event
+        if (d.readyState === "complete") {
+            l();
+        } else {
+            w.addEventListener("load", l, false);
+        }
     }
 })();
