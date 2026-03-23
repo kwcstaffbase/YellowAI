8b4fa72e0e57abadac58f624c08468051a0c1b53
kwcstaffbase <kelvin.cheng@staffbase.com>
2026-03-23 16:32:00 -0600

    Enhance logging and refactor YellowAI script

--- yellowai.js
+++ yellowai.js
@@ -1,16 +1,22 @@
 window.ymConfig = {"bot":"x1765817669787","host":"https://r4.cloud.yellow.ai"};
 (function() {
     var w = window,
-        ic = w.YellowMessenger;
+        d = document;
+
+    console.log("YellowAI script running");
+    console.log("readyState:", d.readyState);
+
+    var ic = w.YellowMessenger;
     if ("function" === typeof ic) {
+        console.log("YellowMessenger already exists, reattaching");
         ic("reattach_activator");
         ic("update", ymConfig);
     } else {
-        var d = document,
-            i = function() {
-                i.c(arguments);
-            };
+        var i = function() {
+            i.c(arguments);
+        };
         function l() {
+            console.log("l() called - injecting widget script");
             var e = d.createElement("script");
             e.type = "text/javascript";
             e.async = true;
@@ -24,11 +30,11 @@ window.ymConfig = {"bot":"x1765817669787","host":"https://r4.cloud.yellow.ai"};
         };
         w.YellowMessenger = i;
 
-        // If the page already loaded, call l() immediately
-        // Otherwise, wait for the load event
         if (d.readyState === "complete") {
+            console.log("Page already complete, calling l() now");
             l();
         } else {
+            console.log("Page not complete, adding load listener");
             w.addEventListener("load", l, false);
         }
     }
